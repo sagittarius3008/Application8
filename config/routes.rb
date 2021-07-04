49b5069d63aed696dadc6271655d1296bca086f5
EC2 Default User <ec2-user@ip-172-31-31-253.ec2.internal>
2021-07-04 01:07:19 +0000

    application5:search

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'books/search' => 'searches#search'
   devise_for :users
   root to: 'homes#top'
   resources :users,only: [:show,:index,:edit,:update,:destroy] do
