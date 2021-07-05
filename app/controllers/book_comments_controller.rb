60f030b0cbd9779043accbd563e820dd21c35ac3
EC2 Default User <ec2-user@ip-172-31-31-253.ec2.internal>
2021-07-05 12:09:35 +0000

    add_ajax:ex_delete

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    
+    @user = User.find_by(id: @book.user_id)
+    # redirect_to book_path(book)
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    # redirect_to book_path(params[:book_id])
   end
 
   private
